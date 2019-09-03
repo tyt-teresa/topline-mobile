5f6243ce136eee2cf8239304dfe623f0ffa748a1
tyt-teresa <1341105528@qq.com>
2019-09-03 20:27:58 +0800

    创建登录组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -10,13 +10,10 @@ export default new Router({
     //   name: '',
     //   component:
     // },
-    // {
-    //   path: '/',
-    //   name: '',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "about" */ '../views/login')
+    }
   ]
 })
