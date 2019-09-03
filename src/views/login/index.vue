f5dbc285ffecc3dc01ff0f307e8e0ab8af22d716
tyt-teresa <1341105528@qq.com>
2019-09-03 21:59:38 +0800

    页面布局及样式实现

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,47 @@
 <template>
-  <div>登录</div>
+  <div>
+    <van-nav-bar title="登录" />
+    <van-cell-group>
+      <van-field left-icon="phone-o" placeholder="请输入手机号" />
+    </van-cell-group>
+    <van-cell-group>
+      <van-field center left-icon="more-o" placeholder="请输入验证码">
+        <van-button slot="button" size="small" type="default" round>获取验证码</van-button>
+      </van-field>
+    </van-cell-group>
+    <div class="login-btn">
+      <van-button type="default">登录</van-button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'login'
+  name: 'login',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.van-cell-group{
+  .van-button{
+    background-color: #ededed;
+  }
+}
+.login-btn {
+  padding: 20px;
+  .van-button {
+    width: 100%;
+    background-color: #6db4fb;
+    border-radius: 5px;
+    color: #fff;
+  }
+}
 </style>
