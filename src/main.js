d78452b888181e53bf577999a21643d53b8778da
tyt-teresa <1341105528@qq.com>
2019-09-04 17:03:50 +0800

    表单验证及加载状态处理

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vant from 'vant'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 import 'vant/lib/index.css'
 import './styles/index.less'
 
 Vue.use(Vant)
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, {
+  events: ''
+})
+Validator.localize('zh_CN', zhCN)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,28 +3,37 @@
     <van-nav-bar title="登录" />
     <van-cell-group>
       <van-field
-      v-validate="required"
+      v-validate="{required:true,regex:/^1[3456789]\d{9}$/}"
       name="mobile"
+      clearable
       v-model="user.mobile"
       left-icon="phone-o"
       placeholder="请输入手机号"
-      error-message=""
+      :error-message="errors.first('mobile')"
       />
     </van-cell-group>
     <van-cell-group>
       <van-field
-      v-validate="required"
+      v-validate="{required:true,regex:/^\d{6}$/}"
       v-model="user.code"
+      clearable
       center
       name="code"
       left-icon="more-o"
       placeholder="请输入验证码"
+      :error-message="errors.first('code')"
       >
         <van-button slot="button" size="small" type="default" round>获取验证码</van-button>
       </van-field>
     </van-cell-group>
     <div class="login-btn">
-      <van-button type="default" @click="onLogin">登录</van-button>
+      <van-button
+      type="default"
+      @click="onLogin"
+      :loading="isLoading"
+      loading-type="spinner"
+      loading-text="正在努力加载中..."
+      >登录</van-button>
     </div>
   </div>
 </template>
@@ -39,13 +48,19 @@ export default {
       user: {
         mobile: '13911111111',
         code: '246810'
-      }
+      },
+      isLoading: false
     }
   },
   methods: {
     ...mapMutations(['setUser']),
     async onLogin () {
       try {
+        const isValid = await this.$validator.validate()
+        if (!isValid) {
+          return
+        }
+        this.isLoading = true
         const data = await login(this.user)
         // 存储登录的状态[vuex(其他外面可以继续读取到),本地(刷新后数据依然保留)]
         // this.$store.commit('setUser', data)
@@ -53,15 +68,28 @@ export default {
         // 跳转到首页并提示成功
         this.$router.push('/')
         this.$toast.fail('登录成功')
+        this.isLoading = false
       } catch (err) {
         if (err.response && err.response.status === 400) {
           this.$toast.fail('手机号码或验证码错误')
+          this.isLoading = false
         }
       }
     }
   },
   created () {
-    // console.log(this)
+    this.$validator.localize('zh_CN', {
+      custom: {
+        mobile: {
+          required: '手机号不能为空',
+          regex: '手机号必须是11位数字'
+        },
+        code: {
+          required: '验证码不能为空',
+          regex: '验证码必须是6位数字'
+        }
+      }
+    })
   }
 }
 </script>
