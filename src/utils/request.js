9f7cf9cb62478c85e6dedb30e4926fca17d8d7c9
tyt-teresa <1341105528@qq.com>
2019-09-04 17:17:49 +0800

    处理token

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import JSONbig from 'json-bigint'
+import store from '@/store'
 // 创建一个axios实例
 const instance = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/'
@@ -14,7 +15,10 @@ instance.defaults.transformResponse = [function (data) {
 }]
 // 设置请求拦截
 instance.interceptors.request.use(function (config) {
-  // Do something before request is sent
+  const { user } = store.state
+  if (user) {
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
   return config
 }, function (error) {
   // Do something with request error
